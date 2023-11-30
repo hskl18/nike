ce25b8a270d2ca074baea71669ed6771c1bbc290
hskl18 <thb182003@gmail.com>
2023-11-29 20:43:30 -0800

    init

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+  return (
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col border-2 justify-center min-h-screen gap-10 border-red-500 p-2 max-container"
+    ></section>
+  );
+};
+
+export default Hero;
